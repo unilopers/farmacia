fb73f668304b9a5d60239b5c3a4c6389ac0593ee
Zambrinn <zambrinfilho@gmail.com>
2025-11-20 16:15:55 -0300

    ♻️ Refactor file structure for Controller

--- API/mvcproject/src/main/java/com/github/zambrinn/mvcproject/controller/UserTableController.java
+++ API/mvcproject/src/main/java/com/github/zambrinn/mvcproject/controller/UserTableController.java
@@ -1,15 +1,24 @@
 package com.github.zambrinn.mvcproject.controller;
 
-import com.github.zambrinn.mvcproject.model.DTOs.UserTableRequest;
-import com.github.zambrinn.mvcproject.model.DTOs.UserTableResponse;
-import com.github.zambrinn.mvcproject.model.service.UserTableService;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.zambrinn.mvcproject.DTOs.UserTableRequest;
+import com.github.zambrinn.mvcproject.DTOs.UserTableResponse;
+import com.github.zambrinn.mvcproject.service.UserTableService;
+
+import jakarta.validation.Valid;
 
 @RequestMapping("/api/v1/UserTable")
 @RestController
