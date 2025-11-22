5eec43da8abca7761a06db6fd04440de65303b30
Mateus Bertani Dias <mateus.dias@horusbi.com.br>
2025-11-22 10:02:50 -0300

    🔥 Remove unused imports

--- API/mvcproject/src/main/java/com/github/zambrinn/mvcproject/service/SaleItemService.java
+++ API/mvcproject/src/main/java/com/github/zambrinn/mvcproject/service/SaleItemService.java
@@ -10,7 +10,6 @@ import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.logging.Logger;
 
 import java.math.BigDecimal;
 import java.util.UUID;
