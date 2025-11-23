19eabea3d2ef648a581f2a7e43e166a9f5b1bc0b
Kauanzizz <kauan.lopes@edu.unifil.br>
2025-11-23 19:13:01 -0300

    ✨ add saleController to repository

--- API/mvcproject/src/main/java/com/github/zambrinn/mvcproject/controller/SaleController.java
+++ API/mvcproject/src/main/java/com/github/zambrinn/mvcproject/controller/SaleController.java
@@ -1,24 +1,38 @@
 package com.github.zambrinn.mvcproject.controller;
 
+import com.github.zambrinn.mvcproject.DTOs.SaleRequest;
 import com.github.zambrinn.mvcproject.DTOs.SaleResponse;
 import com.github.zambrinn.mvcproject.service.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
+@RequestMapping("/api/v1/sale")
 public class SaleController {
-
     @Autowired
     private SaleService saleService;
 
+    @PostMapping
+    public ResponseEntity<SaleResponse> createSale(@RequestBody SaleRequest saleRequest) {
+        return ResponseEntity.ok(saleService.createSale(saleRequest));
+    }
+
     @GetMapping
-    public List<SaleResponse> getSale() {
-        return ResponseEntity.ok(SaleService.getAllSale());
+    public ResponseEntity<List<SaleResponse>> getAllSales() {
+        return ResponseEntity.ok(saleService.getAllSales());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<SaleResponse> updateSale(@PathVariable UUID id, @RequestBody SaleRequest saleRequest) {
+        return ResponseEntity.ok(saleService.updateSale(id, saleRequest));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteSale(@PathVariable UUID id) {
+        saleService.delete(id);
     }
 }
