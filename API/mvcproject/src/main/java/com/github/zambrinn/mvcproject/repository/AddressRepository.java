5e4a1a58e809fecaffc3dc25dc6e3137be1686d5
João Pedro <joaopgf.it@gmail.com>
2025-11-23 16:34:39 -0300

    ✨ create model repository address

---
+++ API/mvcproject/src/main/java/com/github/zambrinn/mvcproject/repository/AddressRepository.java
@@ -0,0 +1,10 @@
+package com.github.zambrinn.mvcproject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, UUID> {
+}
