0ad6340b3847709db937bdf44a3d3f2da1ea4deb
Kauanzizz <kauan.lopes@edu.unifil.br>
2025-11-23 18:00:41 -0300

    ✨start saleController implementation

--- API/mvcproject/src/main/java/com/github/zambrinn/mvcproject/controller/SaleController.java
+++ API/mvcproject/src/main/java/com/github/zambrinn/mvcproject/controller/SaleController.java
@@ -1,5 +1,24 @@
 package com.github.zambrinn.mvcproject.controller;
 
+import com.github.zambrinn.mvcproject.DTOs.SaleResponse;
+import com.github.zambrinn.mvcproject.service.SaleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class SaleController {
 
+    @Autowired
+    private SaleService saleService;
+
+    @GetMapping
+    public List<SaleResponse> getSale() {
+        return ResponseEntity.ok(SaleService.getAllSale());
+    }
 }
