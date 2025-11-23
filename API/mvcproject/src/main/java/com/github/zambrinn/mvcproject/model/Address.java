cf4396393b5772a189db4c4be85511ee2c117a76
João Pedro <joaopgf.it@gmail.com>
2025-11-23 16:34:28 -0300

    ✨ create model address

---
+++ API/mvcproject/src/main/java/com/github/zambrinn/mvcproject/model/Address.java
@@ -0,0 +1,40 @@
+package com.github.zambrinn.mvcproject.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "address")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "street", nullable = false)
+    private String street;
+
+    @Column(name = "number", nullable = false)
+    private String number;
+
+    @Column(name = "city", nullable = false)
+    private String city;
+
+    @Column(name = "state", nullable = false)
+    private String state;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+}
