62922533ad5edbfa7a9023f70ea918f04965314b
Matheus <matheus.passos@edu.unifil.br>
2025-11-23 22:31:59 -0300

    ✨ Add import model address

--- API/mvcproject/src/main/java/com/github/zambrinn/mvcproject/repository/AddressRepository.java
+++ API/mvcproject/src/main/java/com/github/zambrinn/mvcproject/repository/AddressRepository.java
@@ -1,5 +1,6 @@
 package com.github.zambrinn.mvcproject.repository;
 
+import com.github.zambrinn.mvcproject.model.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
