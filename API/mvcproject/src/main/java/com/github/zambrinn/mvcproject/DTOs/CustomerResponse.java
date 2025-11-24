a49e4287e35b4768852760d1693773d295282413
Zambrinn <zambrinfilho@gmail.com>
2025-11-23 22:51:20 -0300

    ✨ Update CustomerResponse to include additional fields for improved customer data representation

--- API/mvcproject/src/main/java/com/github/zambrinn/mvcproject/DTOs/CustomerResponse.java
+++ API/mvcproject/src/main/java/com/github/zambrinn/mvcproject/DTOs/CustomerResponse.java
@@ -1,11 +1,24 @@
 package com.github.zambrinn.mvcproject.DTOs;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public record CustomerResponse(
         UUID id,
+        String name,
+        String cpf,
         String email,
-        LocalDateTime createdAt
+        String phone,
+        LocalDateTime createdAt,
+        List<AddressDTO> addresses  
 ) {
+    public record AddressDTO (
+           UUID id,
+           String street,
+           String state,
+           String city,
+           String number
+    ) {
+    }
 }
