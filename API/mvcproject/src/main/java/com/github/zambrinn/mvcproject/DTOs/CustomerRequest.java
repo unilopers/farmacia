ce0d82873d9c8dd95bdc2ab6300ba0111ab9ad24
Zambrinn <zambrinfilho@gmail.com>
2025-11-23 22:57:14 -0300

    ✨ Update CustomerRequest to include address validation and reformat fields

--- API/mvcproject/src/main/java/com/github/zambrinn/mvcproject/DTOs/CustomerRequest.java
+++ API/mvcproject/src/main/java/com/github/zambrinn/mvcproject/DTOs/CustomerRequest.java
@@ -1,16 +1,24 @@
 package com.github.zambrinn.mvcproject.DTOs;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 
 public record CustomerRequest(
         @NotBlank(message = "The customer must have a name")
         String name,
+        
         @NotBlank(message = "The customer must have a CPF")
         String cpf,
-        @NotBlank(message = "The customer must have a CPF")
+        
+        @NotBlank(message = "The customer must have an email")
         String email,
+        
         @NotBlank(message = "The customer must have a password")
         String passwordHash,
-        String phone
+        
+        String phone,
+        
+        @Valid
+        AddressRequest address
 ) {
 }
