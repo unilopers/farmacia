34eed88473528934fc9d78da35a8f6719a2c1fde
Zambrinn <zambrinfilho@gmail.com>
2025-11-23 22:50:30 -0300

    ✨ Add addresses field to Customer entity for managing customer addresses

--- API/mvcproject/src/main/java/com/github/zambrinn/mvcproject/model/Customer.java
+++ API/mvcproject/src/main/java/com/github/zambrinn/mvcproject/model/Customer.java
@@ -1,11 +1,23 @@
 package com.github.zambrinn.mvcproject.model;
 
-import jakarta.persistence.*;
-import lombok.*;
-
-import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "customer")
@@ -36,4 +48,8 @@ public class Customer {
 
     @Column(name = "created_at")
     private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<Address> addresses = new ArrayList<>();
 }
