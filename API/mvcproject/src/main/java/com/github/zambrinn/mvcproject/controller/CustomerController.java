5e4f2e0f3975d30ab2a0f66fec35f5fd4cde809e
Zambrinn <zambrinfilho@gmail.com>
2025-11-23 21:25:27 -0300

    🩹 Fixing PutMapping and DeleteMapping adding the id route on them

--- API/mvcproject/src/main/java/com/github/zambrinn/mvcproject/controller/CustomerController.java
+++ API/mvcproject/src/main/java/com/github/zambrinn/mvcproject/controller/CustomerController.java
@@ -1,17 +1,24 @@
 package com.github.zambrinn.mvcproject.controller;
 
-import com.github.zambrinn.mvcproject.DTOs.CustomerRequest;
-import com.github.zambrinn.mvcproject.DTOs.CustomerResponse;
-import com.github.zambrinn.mvcproject.model.Customer;
-import com.github.zambrinn.mvcproject.service.CustomerService;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.zambrinn.mvcproject.DTOs.CustomerRequest;
+import com.github.zambrinn.mvcproject.DTOs.CustomerResponse;
+import com.github.zambrinn.mvcproject.service.CustomerService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/Customer")
@@ -29,12 +36,12 @@ public class CustomerController {
         return ResponseEntity.ok(customerService.getAllCustomers());
     }
 
-    @PutMapping
+    @PutMapping("/{id}")
     public ResponseEntity<CustomerResponse> updateCustomer(@PathVariable UUID id, @Valid @RequestBody CustomerRequest request) {
         return ResponseEntity.ok(customerService.updateCustomer(id, request));
     }
 
-    @DeleteMapping
+    @DeleteMapping("/{id}")
     public void deleteCustomer(@PathVariable UUID id) {
         customerService.deleteCustomer(id);
     }
